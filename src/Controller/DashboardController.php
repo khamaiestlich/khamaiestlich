232e4a3bd1415efac7d9031d13b1247afc32f699
khamaies tlich <77756807+khamaiestlich@users.noreply.github.com>
2022-06-17 10:31:06 +0100

    profuit finale

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\WorkerRepository;
+use App\Repository\workCertificateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,4 +24,37 @@ class DashboardController extends AbstractController
             'femme' => $famme
         ]);
     }
+    public function statravail(workCertificateRepository $workCertificateRepository): Response
+    {
+        $January = count($workCertificateRepository->findBy(['m'=> 'F']));
+        $February = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $March = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $April = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $May = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $June = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $Juillet = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $aout = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $septembre = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $october = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $november = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+        $decembre = count($workCertificateRepository->findBy(['Genre'=> 'H']));
+
+        // dd($January, $February, $March, $avril, $May, $june, $juillet, $aout, $septembre, $octobre, $november, $decembre);
+        return $this->render('dashboard/index.html.twig', [
+            '1' => $January,
+            '2' => $February,
+            '3' => $March,
+            '4' => $April,
+            '5' => $May,
+            '6' => $June,
+            '7' => $Juillet,
+            '8' => $aout,
+            '9' => $septembre,
+            '10' => $october,
+            '11' => $november,
+            '12' => $decembre,
+
+        ]);
+    }
+    
 }
